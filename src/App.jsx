c02c0353d31657dfb06a17101619b2e246ac8090
vipinverma <vipinverma4224@gmail.com>
2026-09-16 22:37:44 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-
+import Header from "./sections/Header";
 function App(){
     return (
       <>
-        <div>
-          <h1 className="text-3xl font-bold underline">Hello world!</h1>
-          <h1 className="h1 text-p1">Zenvora</h1>
-        </div>
+       <main className="overflow-hidden">
+       <Header/>
+       </main>
       </>
     );
 }
