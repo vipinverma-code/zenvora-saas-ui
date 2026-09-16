52e4cf684c082a2b0e581d0857b244a71d4c67fe
vipinverma <vipinverma4224@gmail.com>
2026-09-16 20:58:56 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 function App(){
-    return(
-        <>
+    return (
+      <>
         <div>
-            <h1>Saas App Ui</h1>
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
+          <h1 className="h1 text-p1">Zenvora</h1>
         </div>
-        
-        </>
-    )
+      </>
+    );
 }
 export default App;
