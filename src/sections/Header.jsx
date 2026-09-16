13c4c614af0c76274bc9435d0826c5b6be361c5d
vipinverma <vipinverma4224@gmail.com>
2026-09-17 02:03:24 +0530

    changes done

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,28 +1,70 @@
 import React from 'react'
+import { useState } from 'react';
+import {Link as LinkScroll} from 'react-scroll';
+
+function Navlink({title}){
+    return(
+        <LinkScroll
+        className='base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5'>
+        {title}
+        </LinkScroll>
+    )
+}
+
 
 const Header = () => {
+    const[isOpen,setIsOpen] = useState(false);
+
   return (
-    <header className='fixed top-0 left-0 z-50 w-full py-10'>
-        <div className="container flex h-14 items-center max-lg:px-5">
-            <a className="lg:hidden flex-1 cursor-pointer z-2">
-                <img src="/images/zenvora.png" width={115} height={55} alt="logo" />
-            </a>
+    <header className="fixed top-0 left-0 z-50 w-full py-10">
+      <div className="container flex h-14 items-center max-lg:px-5">
+        <a className="lg:hidden flex-1 cursor-pointer z-2">
+          <img src="/images/zenvora.png" width={115} height={55} alt="logo" />
+        </a>
+        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+          <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
+            <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
+              <ul className="flex max-lg:block max-lg:px-12">
+                <li className="nav-li">
+                  <Navlink title={"features"} />
+                  <div className="dot" />
+                  <Navlink title={"pricing"} />
+                </li>
 
-            <div className='w-full border-2 border-amber-400'>
-                <nav>
-                    <ul className='flex max-lg:block max-lg:px-12'>
-                        <li className='nav-li'>
-                           {/* <Navlink/>  */}
+                <li className="nav-logo">
+                  <LinkScroll>
+                    <img
+                      src="/public/images/zenvora.png"
+                      width={160}
+                      height={500}
+                      alt="logo"
+                    />
+                  </LinkScroll>
+                </li>
 
-                        </li>
-
-                    </ul>
-                </nav>
-
-            </div>
+                <li className="nav-li">
+                  <Navlink title={"faq"} />
+                  <div className="dot" />
+                  <Navlink title={"download"} />
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
+        <button
+          className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"
+          onClick={() => setIsOpen((prevState) => !prevState)}
+        >
+          <img src={`/images/${isOpen?"close":"magic"}.svg`}
+            
+            alt="menu"
+            className="size-1/2 object-contain"
+          />
+        </button>
+      </div>
     </header>
-  )
+  );
 }
 
 export default Header
+export {Navlink};
