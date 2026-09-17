5d227943e0a9ea9cb90077ea0751d9387e6ea138
vipinverma <vipinverma4224@gmail.com>
2026-09-17 11:26:51 +0530

    chnages done

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
 import {Link as LinkScroll} from 'react-scroll';
+import clsx from 'clsx';
 
 function Navlink({title}){
     return(
@@ -21,7 +22,13 @@ const Header = () => {
         <a className="lg:hidden flex-1 cursor-pointer z-2">
           <img src="/images/zenvora.png" width={115} height={55} alt="logo" />
         </a>
-        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+
+        <div
+          className={clsx(
+            "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
+            isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none",
+          )}
+        >
           <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
@@ -32,7 +39,15 @@ const Header = () => {
                 </li>
 
                 <li className="nav-logo">
-                  <LinkScroll>
+                  <LinkScroll
+                    to="hero"
+                    offset={-100}
+                    spy
+                    smooth
+                    className={clsx(
+                      "max-lg:hidden transition-transform duration-500 cursor-pointer ",
+                    )}
+                  >
                     <img
                       src="/public/images/zenvora.png"
                       width={160}
@@ -49,14 +64,30 @@ const Header = () => {
                 </li>
               </ul>
             </nav>
+            <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+              <img
+                src="/images/bg-outline.svg"
+                width={960}
+                height={380}
+                alt="outline"
+                className="relative z-2"
+              />
+              <img
+                src="/images/bg-outlines-fill.png"
+                width={960}
+                height={380}
+                alt="outline"
+                className="absolute inset-0 mix-blend-soft-light opacity-5"
+              />
+            </div>
           </div>
         </div>
         <button
           className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"
           onClick={() => setIsOpen((prevState) => !prevState)}
         >
-          <img src={`/images/${isOpen?"close":"magic"}.svg`}
-            
+          <img
+            src={`/images/${isOpen ? "close" : "magic"}.svg`}
             alt="menu"
             className="size-1/2 object-contain"
           />
