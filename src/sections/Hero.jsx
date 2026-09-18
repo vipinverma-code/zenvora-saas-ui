ee080767deac49ca526579175c787a575eb8c7c9
vipinverma <vipinverma4224@gmail.com>
2026-09-18 23:20:00 +0530

    chnages done

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,5 @@
 import { Element, Link as LinkScroll } from "react-scroll";
 import Button from "../components/Button.jsx";
-
 const Hero = () => {
   return (
     <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32">
