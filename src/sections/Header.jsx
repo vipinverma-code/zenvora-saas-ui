935a4e9b8ecdd46158463163c21cf7ff3f57700e
vipinverma <vipinverma4224@gmail.com>
2026-09-21 01:00:35 +0530

    changes done

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,23 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
 import {Link as LinkScroll} from 'react-scroll';
 import clsx from 'clsx';
 
-function Navlink({title}){
-    return(
-        <LinkScroll
-        className='base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5'>
-        {title}
-        </LinkScroll>
-    )
-}
-
-
 const Header = () => {
+    const [hasScrolled, setHasScrolled] = useState(false);
     const[isOpen,setIsOpen] = useState(false);
 
+    useEffect(()=>{
+      const handleScroll = () =>{
+        setHasScrolled(window.scrollY > 32)
+      }
+      window.addEventListener('scroll',handleScroll);
+      return ()=>{
+        window.removeEventListener('scroll',handleScroll);
+      };
+    },[]);
+
+    function Navlink({ title }) {
+      return (
+        <LinkScroll
+          onClick={() => setIsOpen(false)}
+          to={title}
+          offset={-100}
+          spy
+          smooth
+          activeClass="nav-active"
+          className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
+        >
+          {title}
+        </LinkScroll>
+      );
+    }
+
+
   return (
-    <header className="fixed top-0 left-0 z-50 w-full py-10">
+    <header className={clsx("fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]",
+    )}
+    >
       <div className="container flex h-14 items-center max-lg:px-5">
         <a className="lg:hidden flex-1 cursor-pointer z-2">
           <img src="/images/zenvora.png" width={115} height={55} alt="logo" />
@@ -98,4 +118,3 @@ const Header = () => {
 }
 
 export default Header
-export {Navlink}
