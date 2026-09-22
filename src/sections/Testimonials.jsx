517618cfaf28b6cff122c6dbeb105dd048e25d3f
vipinverma <vipinverma4224@gmail.com>
2026-09-23 00:00:45 +0530

    changes done

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { testimonials } from '../constants';
-import TestimonialItem from '../components/TestimonialItem';
+import React from "react";
+import { testimonials } from "../constants";
+import TestimonialItem from "../components/TestimonialItem";
 
 const Testimonials = () => {
-   const halfLength = Math.floor(testimonials.length /2);
+  const halfLength = Math.floor(testimonials.length / 2);
 
   return (
     <>
       <section className="relative z-2 py-24 md:py-28 lg:py-40">
         <div className="container block lg:flex">
-          <div className="testimonials_head-res relative z-2 mr-20 flex-300 ">
+          <div className="testimonials_head-res relative z-2 mr-20 flex-300">
             <p className="caption mb-5 max-md:mb-2.5">Wall of Love</p>
-            <h3 className="h3 max-md:h5 text-p4 ">Words from our fans </h3>
+            <h3 className="h3 max-md:h5 text-p4">Words from our fans</h3>
           </div>
           <div className="testimonials_inner-after testimonials_inner-before relative -my-12 -mr-3 flex items-start max-lg:static max-md:block">
             <div className="testimonials_group-after flex-50">
@@ -37,6 +37,6 @@ const Testimonials = () => {
       </section>
     </>
   );
-}
+};
 
 export default Testimonials;
