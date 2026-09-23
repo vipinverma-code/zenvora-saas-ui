d53c3ee77932f5a9e9f512049a7e4e41d9ec7ecd
vipinverma <vipinverma4224@gmail.com>
2026-09-23 23:11:05 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Hero from "./sections/Hero";
 import Features from "./sections/Features";
 import Pricing from "./sections/Pricing";
 import Faq from "./sections/Faq";
+import Testimonials from "./sections/Testimonials";
+import Download from "./sections/Download";
 function App(){
     return (
       <>
@@ -12,6 +14,8 @@ function App(){
        <Features/>
        <Pricing/>
        <Faq/>
+       <Testimonials/>
+       <Download/>
        </main>
       </>
     );
